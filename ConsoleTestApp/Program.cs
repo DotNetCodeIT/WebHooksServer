713e914f6fe5a6831a34c4f8fad38e03fb4f99ed
Luca Congiu <congiuluc@hotmail.com>
2017-03-23 19:03:26 +0100

    PreRelease

---
+++ ConsoleTestApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+
+namespace ConsoleTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            do
+            {
+                Console.WriteLine("Inserisci il tuo nome:");
+                string nome = Console.ReadLine();
+                using (HttpClient client = new HttpClient())
+                {
+                    string baseUrl = "https://azurefunct1.azurewebsites.net";
+                    string requestUrl = "api/TestHttpTrigger?code=8xhDzFXic3fxvTb0UgqEa0vBtESCh9indEmgDSdbab39/AoxSQr1oQ==";
+                    string jsonData = $"{{name:'{nome}'}}";
+
+                    client.BaseAddress = new Uri(baseUrl);
+
+                    HttpContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+                    HttpResponseMessage result = client.PostAsync(requestUrl, content).Result;
+
+                    if (result.IsSuccessStatusCode)
+                    {
+                        Console.WriteLine(result.Content.ReadAsStringAsync().Result);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error: {0}", result.StatusCode);
+                    }
+                }
+                Console.WriteLine("Scrivi exit per uscire");
+            } while (Console.ReadLine()!="exit");
+          
+
+            
+
+        }
+    }
+}
